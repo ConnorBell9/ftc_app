8654dad7ce69ca41580080498052c3e6d9daadcd
Rewinder73 <ethanxboone@gmail.com>
2017-10-30 20:27:57 -0400

    Change the autonomous name

--- Team11750/src/main/java/org/firstinspires/ftc/team11750/BellatorumAutoRight.java
+++ Team11750/src/main/java/org/firstinspires/ftc/team11750/BellatorumAutoRight.java
@@ -55,7 +55,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Bellatorum: Auto Drive By Time", group="Bellatorum")
+@Autonomous(name="Bellatorum: Auto Right", group="Bellatorum")
 public class BellatorumAutoRight extends LinearOpMode {
 
     /* Declare OpMode members. */
