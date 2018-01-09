6dfa663cb5e5a0acd803576fc59d023ae86e5ec9
Tyler Keeney <keeps25tk@gmail.com>
2018-01-09 13:18:38 -0500

    Update RedRightByrd.java

--- Team7153/src/main/java/org/firstinspires/ftc/team7153/RedRightByrd.java
+++ Team7153/src/main/java/org/firstinspires/ftc/team7153/RedRightByrd.java
@@ -30,10 +30,10 @@ public class RedRightByrd extends AutoByrdMK2 {
             moveWithEncoders(14,.3,FORWARDS);
             turn(TURN_BACK,.3);
             moveToCubby(MOVE_BACKWARDS, VUFORIA_ENABLED);
-            /*while(runTime.milliseconds()<25000 && opModeIsActive()){
+            while(runTime.milliseconds()<25000 && opModeIsActive()){
             	harvest(60,24);
             	moveToCubby(MOVE_BACKWARDS, VUFORIA_DISABLED);
-			}*/
+			}
 			stopMoving();
             intake(false);
 		}
