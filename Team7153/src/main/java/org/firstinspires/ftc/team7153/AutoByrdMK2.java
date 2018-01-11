b53f8f4be51442023775614ae0ec4dbe0ae36cbc
Tyler Keeney <keeps25tk@gmail.com>
2018-01-11 15:14:37 -0500

    Update AutoByrdMK2.java

--- Team7153/src/main/java/org/firstinspires/ftc/team7153/AutoByrdMK2.java
+++ Team7153/src/main/java/org/firstinspires/ftc/team7153/AutoByrdMK2.java
@@ -114,7 +114,7 @@ public class AutoByrdMK2 extends LinearOpMode {
 		//Reset the timer to 0
 		runTime.reset();
 
-		//Tell the robot that the Gyroscope has been correctly calibrated
+		//Tell the robot that the Gyroscope has been correctly calibrated (Not used yet)
 		IS_GYRO_ON = true;
 
 		statusCheck();
@@ -124,14 +124,22 @@ public class AutoByrdMK2 extends LinearOpMode {
 		if(isStopRequested()){
 			return;
 		}
+		/* Moves the clamp to either
+		*CLAMP_POSITION_1 (Which is ground level)
+		*CLAMP_POSITION_2 (Which is the position for the second block)
+		*CLAMP_POSITION_3 (Which is the position for the third block)
+		*CLAMP_POSITION_4 (Which is the position for the fourth block)
+		*/
 		robot.clamp.setTargetPosition(position);
 	}
 	
 	void grab(boolean grab) {
-		//If the argument is true then the clamp will close otherwise the clamp will return to its original orientation
 		if(isStopRequested()){
 			return;
 		}
+		/*If grab is true then the clamps will move to their respective close positions
+		*otherwise they will move to their respective open positions
+		*/
 		if (grab) {
 			robot.armL.setPosition(LEFT_CLAMP_CLOSE);
 			robot.armR.setPosition(RIGHT_CLAMP_CLOSE);
@@ -147,58 +155,78 @@ public class AutoByrdMK2 extends LinearOpMode {
 		if(isStopRequested()){
 			return;
 		}
-		//Bring the hammer down and wait in order to get around the shakiness of the hammer
+		//Brings the hammer down and turns on the LED
 		robot.color.enableLed(true);
 		robot.hammerY.setPosition(HAMMER_DOWN);
 		sleep(1000);
 		if(isStopRequested()){
 			return;
 		}
-		//If the color red is greater than the color blue then if the arguement is red it will putt the blue ball off (Left) otherwise it will putt the red ball off (Right)
+		/*If the color red is greater than the color blue
+		*then, if the arguement is red it will putt the blue ball off (Left)
+		*otherwise it will putt the red ball off (Right)
+		*/
 		if(robot.color.red()>robot.color.blue()){
 			telemetry.addData("Found Color: ", "red");
 			telemetry();
 			if(colorRemaining==RED){putt(RIGHT);} else {putt(LEFT);}
 		} else if (robot.color.red()<robot.color.blue()){
-			//Else if the color red is less than the color blue then if the argument is red it will put the blue ball off (Right) otherwise it will putt the red ball off (Left)
+			/*If the color red is less than the color blue
+			*then, if the arguement is red it will putt the blue ball off (Right)
+			*otherwise it will putt the red ball off (Left)
+			*/
 			telemetry.addData("Found Color: ", "blue");
 			telemetry();
 			if(colorRemaining==RED){putt(LEFT);} else {putt(RIGHT);}
 		} else {
+			//If no color is found then nothing will occur
 			telemetry.addData("Found Color: ", "Nothing");
 			telemetry();
 		}
 		statusCheck();
 		sleep(1000);
 		statusCheck();
-		//Reset the hammer position to up and turn off the light
+		//Reset the hammer position to up and turn off the LED
 		robot.hammerY.setPosition(HAMMER_UP);
 		robot.hammerX.setPosition(HAMMER_CENTER);
 		robot.color.enableLed(false);
 	}
 
 	void harvest(double X_AXIS, double Y_AXIS) throws InterruptedException{
-		X_AXIS+=OFFSET;
 		turn(TURN_RIGHT,.3);
+		//Open up the clamp and activate the intake in preparation of blocks.
 		grab(false);
 		intake(true);
+		//Move away from the wall for Y_AXIS inches.
 		moveWithEncoders(Y_AXIS,.3,FORWARDS);
+		/* X_AXIS is based off of the bot's forward direction.
+		* Forwards is positive.
+		* Backwards is negative.
+		* So if X_AXIS is a positive number then it will turn forwards functions.
+		* If the X_AXIS is a negative number then it will turn backwards.
+		* If the robot doesn't have to move in the X direction then it will assume it has 
+		* reached its destination and attempt to pick up a block.
+		* After looping the first time it will then correct for the bot's offset.
+		*
+		*/
+		boolean block=true;
+		for(int i=0; i<2; i++){
 		if(X_AXIS>0){
 			turn(TURN_FORWARDS,.3);
-			moveWithEncoders(X_AXIS,.3,FORWARDS);
-			grab(true);
-			clamp(CLAMP_POSITION_2);
-			moveWithEncoders(X_AXIS,.3,BACKWARDS);
+			moveWithEncoders(X_AXIS,.3,block);
 		} else if (X_AXIS<0) {
 			turn(TURN_BACK,.3);
-			moveWithEncoders(-X_AXIS,.3,FORWARDS);
-			grab(true);
-			clamp(CLAMP_POSITION_2);
-			moveWithEncoders(-X_AXIS,.3,BACKWARDS);
-		} else{
-			grab(true);
-			clamp(CLAMP_POSITION_2);
+			moveWithEncoders(-X_AXIS,.3,block);
 		}
+		grab(true);
+		clamp(CLAMP_POSITION_2);
+		//Add the offset incurred by placing the cryptoblock
+		X_AXIS-=OFFSET;
+		boolean block=false;
+		}
+		/* After picking up the block deactivate the intake system.
+		* Then return using the Y_AXIS now going back towards the cryptobox
+		*/
 		intake(false);
 		turn(TURN_RIGHT,.3);
 		moveWithEncoders(Y_AXIS,.3,BACKWARDS);
@@ -280,6 +308,7 @@ public class AutoByrdMK2 extends LinearOpMode {
 		//While the hammer is not detecting any color or the time-out hasn't occurred the robot will move towards a cubby slot
 		if(!SLOT_2 && !mode){
 			SLOT_2=true;
+			OFFSET = 0;
 			/*moveWithEncoders(14.5,.3,color);
 			SLOT_2 = true;
 			if(color==RED){
@@ -300,7 +329,7 @@ public class AutoByrdMK2 extends LinearOpMode {
 ////STRAIGHT AUTO
 		} else if ((relicVuMark == RelicRecoveryVuMark.LEFT && mode) || (!SLOT_1 && !mode)) {
 			SLOT_1 = true;
-			OFFSET = -7.63;
+			if(!color){OFFSET = -7.63;}else{OFFSET = 7.63}
 			/*moveWithEncoders(7.25,.3,!color);
 			if(color==RED){
 				turn(TURN_LEFT+45,.3);
@@ -322,7 +351,7 @@ public class AutoByrdMK2 extends LinearOpMode {
 
 		} else if ((relicVuMark == RelicRecoveryVuMark.RIGHT && mode) || (!SLOT_3 && !mode)) {
 			SLOT_3 = true;
-			OFFSET = 7.63;
+			if(!color){OFFSET = 7.63;}else{OFFSET = -7.63}
 			/*moveWithEncoders(7.25,.3,color);
 			SLOT_2 = true;
 			if(color==RED){
@@ -472,14 +501,14 @@ public class AutoByrdMK2 extends LinearOpMode {
 		}
 		//Sets the angle that the robot is supposed to be in to the angle arguement
 		imaginaryAngle = angle;
-		//While the angel is > the gyroscope+2 or < the gyroscope-2
-		//while(angle > (robot.gyro.getHeading()+2)%360 || angle < robot.gyro.getHeading()-2){
+		//While the angel is > the gyroscope+TURN_ERROR or < the gyroscope-TURN_ERROR
 		resetTimer();
 		while((robot.gyro.getHeading()<angle-TURN_ERROR || robot.gyro.getHeading()>angle+TURN_ERROR) && (angle-TURN_ERROR<=-1 && robot.gyro.getHeading() != 360-TURN_ERROR || angle-TURN_ERROR>-1) && (angle+TURN_ERROR>=360 && robot.gyro.getHeading()>TURN_ERROR-1 || angle+TURN_ERROR<360) && INPUT_TIMER+5000>runTime.milliseconds()){
 			if(isStopRequested()){
 				stopMoving();
 				return;
 			}
+			//Checks to see if turning left or right
 		    if((angle>robot.gyro.getHeading() && angle<robot.gyro.getHeading()+181) || (angle<robot.gyro.getHeading()-180)){
                 	robot.frontLeft.setPower(-speed);
                 	robot.frontRight.setPower(speed);
