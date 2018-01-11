a1618607ccc7a21933fc1df90f153860c2cbbeb2
Tyler Keeney <keeps25tk@gmail.com>
2018-01-11 14:13:01 -0500

    Update BlueLeftByrd.java

--- Team7153/src/main/java/org/firstinspires/ftc/team7153/BlueLeftByrd.java
+++ Team7153/src/main/java/org/firstinspires/ftc/team7153/BlueLeftByrd.java
@@ -26,12 +26,11 @@ public class BlueLeftByrd extends AutoByrdMK2 {
             moveWithEncoders(24,.3,FORWARDS);
             turn(TURN_RIGHT,.3);
             moveWithEncoders(14,.3,FORWARDS);
-            turn(TURN_FORWARDS,.3);
             moveToCubby(BLUE, VUFORIA_ENABLED);
-            /*while(runTime.milliseconds()<25000 && opModeIsActive()){
+            while(runTime.milliseconds()<20000 && opModeIsActive()){
             	harvest(60,24);
             	moveToCubby(MOVE_BACKWARDS, VUFORIA_DISABLED);
-			}*/
+			}
 			stopMoving();
             intake(false);
 		}
