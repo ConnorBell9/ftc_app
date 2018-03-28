0a296a2816abc37dfbb473d1bbe5c6b319f3d4ed
Tyler Keeney <keeps25tk@gmail.com>
2018-03-28 12:41:38 -0400

    Add files via upload

---
+++ TeamInventum/src/main/java/org/firstinspires/ftc/teamInventum/TankDriveCodeEd.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamInventum;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="TankDriveCodeEd")
+public class TankDriveCodeEd extends OpMode{
+    @Override
+    public void init() {
+    
+    }
+
+    @Override
+    public void loop() {
+	    
+    }
+}
