007ca385a87f822cef706f2324f76c21ca1e27b9
Tyler Keeney <keeps25tk@gmail.com>
2018-09-04 10:19:27 -0400

    Update tylerDance.java

--- Team11750/src/main/java/org/firstinspires/ftc/team11750/tylerDance.java
+++ Team11750/src/main/java/org/firstinspires/ftc/team11750/tylerDance.java
@@ -45,24 +45,39 @@ public class Teleop extends OpMode{
 
     @Override
     public void loop() {
-        
-        double maxSpeed = 1;//Defines what fraction of speed the robot will run at
-		//double radGyro = (robot.gyro.getHeading() * Math.PI) / 180;
-	    double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-		double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-	    if(gamepad1.right_bumper){robotAngle=Math.PI*7/4; r=1;}
-	    else if (gamepad1.left_bumper){robotAngle=Math.PI*3/4; r=1;}
-	    double rightX = gamepad1.right_stick_x;
-	    final double v1 = r * Math.sqrt(2) * Math.cos(robotAngle) + rightX;
-	    final double v2 = r * Math.sqrt(2) * Math.sin(robotAngle) - rightX;
-	    final double v3 = r * Math.sqrt(2) * Math.sin(robotAngle) + rightX;
-	    final double v4 = r * Math.sqrt(2) * Math.cos(robotAngle) - rightX;
+    	double maxSpeed = 1;//Defines what fraction of speed the robot will run at
+	double maxTurn = .5; 
+	double ratio = 0;
+	    
+	double radGyro = (robot.gyro.getHeading() * Math.PI) / 180;
+	double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y)*maxSpeed;
+	double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+    	double turn = gamepad1.right_stick_x*Math.abs((maxTurn-1)/(maxTurn+1));
 
-	    robot.frontLeft.setPower(v1*maxSpeed);
-		robot.frontRight.setPower(v2*maxSpeed);
-		robot.backLeft.setPower(v3*maxSpeed);
-		robot.backRight.setPower(v4*maxSpeed);
-        
+    	double frontLeft = Math.sqrt(2)*Math.cos(robotAngle + radGyro) + turn;
+    	double frontRight = Math.sqrt(2)*Math.sin(robotAngle + radGyro) - turn;
+    	double backLeft = Math.sqrt(2)*Math.sin(robotAngle + radGyro) + turn;
+	double backRight = Math.sqrt(2)*Math.cos(robotAngle + radGyro) - turn;
+
+    	if(Math.abs(frontLeft)>ratio) {
+      		ratio=Math.abs(frontLeft);
+    	} if(Math.abs(frontRight)>ratio){
+      		ratio=Math.abs(frontRight);
+    	} if(Math.abs(backRight)>ratio){
+      		ratio=Math.abs(backRight);
+    	} if(Math.abs(backLeft)>ratio){
+      		ratio=Math.abs(backLeft);
+    	}
+    	frontLeft/=ratio;
+    	frontRight/=ratio;
+    	backLeft/=ratio;
+    	backRight/=ratio;
+	    
+    	System.out.println(gyro);
+    	robot.frontLeft.setPower(r*frontLeft);
+    	robot.frontRight.setPower(r*frontRight);
+    	robot.backLeft.setPower(r*backRight);
+    	robot.backRight.setPower(r*backLeft);
     }
 
     @Override
